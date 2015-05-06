f8039910ff940dca8648f30acbb9dbf9abce5a12
bcleenders <bcleenders@gmail.com>
2015-05-07 00:29:56 +0200

    some hacky stuff

--- backend/plugins/passportSAML.js
+++ backend/plugins/passportSAML.js
@@ -1,67 +1,67 @@
-var passport = require('passport');
-var passportSAML = require('passport-saml');
-
-var myPlugin = {
-    register: function (server, options, next) {
-
-        passport.initialize();
-
-        var SamlStrategy = passportSAML.Strategy;
-
-        passport.use('saml', new SamlStrategy({
-                path: '/login/saml/callback',
-                entryPoint: 'https://openidp.feide.no/simplesaml/saml2/idp/SSOService.php',
-                issuer: 'passport-saml'
-            },
-            function (profile, done) {
-                debugger;
-                console.log('Profile: ');
-                console.log(profile);
-            })
-        );
-
-        /*
-         Define a custom authentication strategy.
-         This authenticates with SAML and gives a token on successful login.
-         */
-        var scheme = function (server, options) {
-            return {
-                authenticate: function(request, reply, opts) {
-                    passport.authenticate('saml')({
-                        query: request.query,
-                        body: request.body || request.payload,
-                        session: request.session
-                    }, reply, next);
-                }
-            };
-        };
-
-        server.auth.scheme('samlScheme', scheme);
-        server.auth.strategy('saml', 'samlScheme');
-
-        console.log('Registering /login/saml');
-
-        server.route({
-            method: 'GET',
-            // The callback endpoint registered with the provider
-            path: '/login/saml',
-            config: {
-                auth: 'saml',
-                handler: function (req, reply) {
-                    debugger;
-                    // Give user a token
-                    reply('TOKEN!');
-                }
-            }
-        });
-
-        next();
-    }
-};
-
-myPlugin.register.attributes = {
-    name: 'myPlugin',
-    version: '1.0.0'
-};
-
-module.exports = myPlugin;
+//var passport = require('passport');
+//var passportSAML = require('passport-saml');
+//
+//var myPlugin = {
+//    register: function (server, options, next) {
+//
+//        passport.initialize();
+//
+//        var SamlStrategy = passportSAML.Strategy;
+//
+//        passport.use('saml', new SamlStrategy({
+//                path: '/login/saml/callback',
+//                entryPoint: 'https://openidp.feide.no/simplesaml/saml2/idp/SSOService.php',
+//                issuer: 'passport-saml'
+//            },
+//            function (profile, done) {
+//                debugger;
+//                console.log('Profile: ');
+//                console.log(profile);
+//            })
+//        );
+//
+//        /*
+//         Define a custom authentication strategy.
+//         This authenticates with SAML and gives a token on successful login.
+//         */
+//        var scheme = function (server, options) {
+//            return {
+//                authenticate: function(request, reply, opts) {
+//                    passport.authenticate('saml')({
+//                        query: request.query,
+//                        body: request.body || request.payload,
+//                        session: request.session
+//                    }, reply, next);
+//                }
+//            };
+//        };
+//
+//        server.auth.scheme('samlScheme', scheme);
+//        server.auth.strategy('saml', 'samlScheme');
+//
+//        console.log('Registering /login/saml');
+//
+//        server.route({
+//            method: 'GET',
+//            // The callback endpoint registered with the provider
+//            path: '/login/saml',
+//            config: {
+//                auth: 'saml',
+//                handler: function (req, reply) {
+//                    debugger;
+//                    // Give user a token
+//                    reply('TOKEN!');
+//                }
+//            }
+//        });
+//
+//        next();
+//    }
+//};
+//
+//myPlugin.register.attributes = {
+//    name: 'myPlugin',
+//    version: '1.0.0'
+//};
+//
+//module.exports = myPlugin;
--- backend/server.js
+++ backend/server.js
@@ -38,9 +38,56 @@ for (var i = 0; i < routes.length; i++) {
 }
 
 
-server.register(require('./plugins/passportSAML'), function (err) {
-    if (err) {
-        console.log('Failed loading HapiMongoModels plugin: ' + err.toString());
+//server.register(require('./plugins/passportSAML'), function (err) {
+//    if (err) {
+//        console.log('Failed loading HapiMongoModels plugin: ' + err.toString());
+//    }
+//});
+var samlStrat = require("passport-saml").Strategy,
+    samlLogin = require("hapi-passport")(new samlStrat({
+            path: '/login/callback',
+            entryPoint: 'https://openidp.feide.no/simplesaml/saml2/idp/SSOService.php',
+            issuer: 'passport-saml'
+        },
+        function (profile, done) {
+            debugger;
+            console.log('Profile: ');
+            console.log(profile);
+
+            done(null, {name: 'John'});
+        })
+    );
+
+// Beware: only works if you set
+//  callbackUrl = 'http://localhost:3000/login/saml';
+// in passport-saml/strategy.js
+
+server.route({
+    method: '*',
+    path: "/login/callback",
+    handler: samlLogin({
+        onSuccess: function (info, request, reply) {
+            // maybe do a redirect?
+            reply('DONE');
+        },
+        onFailed: function (warning, request, reply) {
+            // maybe show an error?
+            reply('DONE2');
+        },
+        onError: function (error, request, reply) {
+            // tell the world that you are angry.
+            debugger;
+            console.log(error);
+            console.log(error.stack);
+            reply('DONE3');
+        }
+    })
+});
+
+server.route({
+    method: "GET", path: "/login/saml/callback",
+    handler: function(req, reply) {
+        reply('You got redirected!');
     }
 });
 
