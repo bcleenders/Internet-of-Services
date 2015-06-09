679a6d26f13ae1d9dd30ca8a20ffcd731dac23f2
bcleenders <bcleenders@gmail.com>
2015-06-09 19:32:02 +0200

    Add some basic course preferences

--- migrations/20150513161556_create_group.js
+++ migrations/20150513161556_create_group.js
@@ -4,7 +4,7 @@ exports.up = function(knex) {
   return knex.schema.createTable('groups', function(table) {
     table.increments('id').primary();
     table.string('name');
-    table.integer('course_id').notNullable().references('courses.id')
+    table.integer('course_id').notNullable().references('courses.id');
     table.boolean('waitingList').notNullable().defaultTo(false);
     table.timestamps();
   });
